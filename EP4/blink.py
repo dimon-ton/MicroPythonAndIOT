a624bc9650a9be6c8e594defba678cfbf425fe5d
dimon-ton <saichon2namoom@gmail.com>
2022-05-25 19:48:07 +0700

    EP4 blinking complete

---
+++ EP4/blink.py
@@ -0,0 +1,16 @@
+from machine import Pin
+ import time
+ led = Pin(14,Pin.OUT)
+ led.on()
+ led.off()
+
+def blink(t):
+    time.sleep(t)
+    led.on()
+    time.sleep(t)
+    led.off()
+    
+blink(0.5)
+
+for i in range(10):
+    blink(0.5)
